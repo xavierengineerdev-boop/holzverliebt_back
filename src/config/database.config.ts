937b7d710107409d8d4457b50a99ca18ffcbaa08
ihorhnennyi <ihorhnennyi@gmail.com>
2026-01-29 12:52:14 +0200

    update back seed

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,7 +1,7 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('database', () => ({
-  uri: process.env.MONGODB_URI || 'mongodb://localhost:27017/back-shop',
+  uri: process.env.MONGODB_URI || 'mongodb://localhost:27017/holzverliebt_bd',
 }));
 
 
